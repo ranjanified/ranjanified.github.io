f60f41ac48975a7655007581950c87af5a302070
Nalin Ranjan <nranjan@ivycomptech.com>
2023-06-18 22:07:06 +0530

    Circular progress now aligns itself in the center of it's parent.

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,16 @@
-<div>
-  Build In Progress
-</div>
+<script lang="ts">
+ import { onMount } from "svelte";
+
+ let mounted = false;
+ onMount(() => {
+   setTimeout(() => {
+     dispatchEvent(new CustomEvent("ranjanified:app:loaded"));
+     mounted = true;
+   }, 3000);
+ });
+</script>
+{#if mounted}
+  <div>
+    Build In Progress
+  </div>
+{/if}
