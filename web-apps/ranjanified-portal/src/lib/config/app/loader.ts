3f4aa0b7d49a2f10a6847829ee979162d5adfc5d
Nalin Ranjan <nranjan@ivycomptech.com>
2023-06-21 20:11:59 +0530

    App Config loader using VITE_APP_CONFIG_ENDPOINT env variable.

--- web-apps/ranjanified-portal/src/lib/config/app/loader.ts
+++ web-apps/ranjanified-portal/src/lib/config/app/loader.ts
@@ -1,7 +1,7 @@
 import { AppConfiguration } from "./app-model";
 
 export const load_config = async (): Promise<AppConfiguration> => {
-    const config: AppConfiguration = await (await fetch(`${document.baseURI}/config/app.json`)).json();
+    const config: AppConfiguration = await (await fetch(import.meta.env.VITE_APP_CONFIG_ENDPOINT)).json();
 
     return config;
 }
