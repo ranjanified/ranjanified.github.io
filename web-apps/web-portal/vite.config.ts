7cde08167432fdb815d321202e6d10bc801b4e87
Nalin Ranjan <nranjan@ivycomptech.com>
2023-06-22 14:24:16 +0530

    Application ranjanified-portal renamed to web-portal.

--- web-apps/web-portal/vite.config.ts
+++ web-apps/web-portal/vite.config.ts
@@ -5,13 +5,13 @@ import { svelte } from "@sveltejs/vite-plugin-svelte";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [tsconfigPaths(), svelte()],
-  base: "/web-assets/ranjanified-portal/",
+  base: "/web-assets/web-portal/",
   // root: "src",
   build: {
     manifest: true,
     // ssrManifest: true,
     emptyOutDir: true,
-    outDir: "../../web-assets/ranjanified-portal",
+    outDir: "../../web-assets/web-portal",
     rollupOptions: {
       preserveEntrySignatures: "strict",
       input: {
